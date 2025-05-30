6f829cf874c45bca489b4ccf4da19d11f90df937
Kshat111 <akshatsinghiitm@gmail.com>
2025-05-30 15:17:21 +0530

    TT maaf kardo

--- src/components/home/HomePage.jsx
+++ src/components/home/HomePage.jsx
@@ -128,6 +128,9 @@ const HomePage = () => {
               <SocialLink href="https://www.instagram.com/axatthisside/" target="_blank" rel="noopener noreferrer">
                 <InstagramIcon />
               </SocialLink>
+              <SocialLink href="mailto:akshatsingh22084@gmail.com" target="_blank" rel="noopener noreferrer">
+            <GmailIcon />
+          </SocialLink>
             </SocialLinks>
             
             <CurrentPositionWrapper>
@@ -309,7 +312,11 @@ const InstagramIcon = () => (
   </svg>
 );
 
-
+const GmailIcon = () => (
+  <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
+    <path d="M20 18h-2V9.25L12 13 6 9.25V18H4V6h1.2l6.8 4.25L18.8 6H20m0-2H4c-1.11 0-2 .89-2 2v12a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2z"/>
+  </svg>
+);
 
 const HomeContainer = styled.div`
   overflow: hidden;
